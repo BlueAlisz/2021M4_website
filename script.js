c6a82d137b69528f51de32592b240abb7343f514
BlueAlisz <nattaphat_y@cmu.ac.th>
2021-05-28 16:09:46 +0700

    commit - alert box

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ var newText = 'new Text'
 document.getElementById('ok').innerHTML = newText
 var myName = 'Nattaphat'
 document.getElementById('no').innerHTML = myName
+
+function myFunction() {
+    alert("Hello\nHow are you?");
+  }
